5ec58e7a2733476671f2e0d53266b3ef856bf816
AerenN <aerenbaysal@gmail.com>
2022-01-27 19:53:41 +0300

    First commit

---
+++ src/main/java/com/blog/Blog/BlogDenemeApplication.java
@@ -0,0 +1,13 @@
+package com.blog.Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDenemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDenemeApplication.class, args);
+	}
+
+}
